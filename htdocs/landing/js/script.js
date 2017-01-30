ece112bb824778b2f59305609b3ef4c48561afe0
Yordan Lasonov <hello@leapfunction.com>
2017-01-30 23:10:48 +0200

    More progress

--- htdocs/landing/js/script.js
+++ htdocs/landing/js/script.js
@@ -8,6 +8,16 @@ var $ = require('jquery');
 window.$ = $;
 window.jQuery = $;
 
+$('nav li ul').hide().removeClass('fallback');
+$('nav li').hover(
+    function () {
+        $('ul', this).stop().slideDown(300);
+    },
+    function () {
+        $('ul', this).stop().slideUp(300);
+    }
+);
+
 /* Changing the opacity of the menu while scrolling */
 $(window).bind('scroll', function () {
     if ($(window).scrollTop() > 603) {
@@ -17,7 +27,7 @@ $(window).bind('scroll', function () {
     }
 });
 
-
+/* Fading the header contain for better outlook */
 $(window).scroll(function () {
     var scrollTop, height;
     scrollTop = $(window).scrollTop();
