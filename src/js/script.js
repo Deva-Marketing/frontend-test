824c52908afcaba9f37fd19ec35cf363c1f35191
Yordan Lasonov <hello@leapfunction.com>
2017-02-03 18:45:46 +0200

    Moar progress

--- src/js/script.js
+++ src/js/script.js
@@ -7,16 +7,6 @@ var $ = require('jquery');
 window.$ = $;
 window.jQuery = $;
 
-$('nav li ul').hide().removeClass('fallback');
-$('nav li').hover(
-    function () {
-        $('ul', this).stop().slideDown(300);
-    },
-    function () {
-        $('ul', this).stop().slideUp(300);
-    }
-);
-
 /* Changing the opacity of the menu while scrolling */
 $(window).bind('scroll', function () {
     if ($(window).scrollTop() > 603) {
@@ -32,4 +22,18 @@ $(window).scroll(function () {
     scrollTop = $(window).scrollTop();
     height = 400;
     $('.header-container').css({'opacity': ((height - scrollTop) / height)});
+});
+
+/* Testamonail Carousel */
+$(function () {
+    var items = 0;
+    $('.testimonials li').hide().eq(items).show();
+
+    function next() {
+        $('.testimonials li:visible').delay(5000).fadeOut('slow', function () {
+            $(this).appendTo('.testimonials ul');
+            $('.testimonials li:first').fadeIn('slow', next);
+        });
+    }
+    next();
 });
